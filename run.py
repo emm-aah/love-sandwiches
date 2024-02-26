ec88fb077c62cdbcfc79e2f9ea623311800d83df
Emma Fox <emmajfox90@gmail.com>
2024-02-26 13:48:31 +0000

    get data from sales column

--- run.py
+++ run.py
@@ -82,6 +82,18 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Gets a list of the last 5 entries from the sales column 
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
 
 def main():
     """
@@ -94,4 +106,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches data autonmation")
-main()
+# main()
+
+get_last_5_entries_sales()
